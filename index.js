df9726982f9ea2b6980b3a75ba147f11caa2bd8d
Biliana <bilianavaleva@gmail.com>
2022-01-09 23:23:40 +0100

    Update main file with game play

---
+++ index.js
@@ -0,0 +1,12 @@
+// Using a function
+// const play = require('./discarded-examples/game-func');
+// const getPrompt = require('./discarded-examples/get-prompt');
+// const prompt = getPrompt();
+
+// play(prompt);
+
+// Using a class
+const Game = require('./game');
+const game = new Game();
+
+game.play();
