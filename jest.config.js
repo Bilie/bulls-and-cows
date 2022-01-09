c4af393434cd26543663ef74e9f48fd93183e265
Biliana <bilianavaleva@gmail.com>
2022-01-10 00:41:33 +0100

    Update jest config with comment for discarded examples

--- jest.config.js
+++ jest.config.js
@@ -155,6 +155,8 @@ module.exports = {
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
     "/node_modules/",
+    // Remove if you want to run tests from discarded folder
+    // warning: some tests in there are quite slow
     "/discarded-examples/"
   ],
 
